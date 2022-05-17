e1e4dff047d2b6123d07bbbd09eb90a0cad6cba5
NamitsGit <73180333+NamitsGit@users.noreply.github.com>
2022-05-17 10:25:27 +0530

    Update call_fitzcli.py

--- call_fitzcli.py
+++ call_fitzcli.py
@@ -5,8 +5,3 @@ filepath = 'statement_sample1-Copy1.PDF' # Can change the filename HERE
 cmd = 'python3 fitzcli.py gettext '+filepath
 
 p = subprocess.Popen(cmd)
-# out, err = p.communicate()
-# result = out.split('\n')
-# for lin in result:
-#     if not lin.startswith('#'):
-#         print(lin)
