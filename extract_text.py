2e1945ab10b956d1cfcb13c4f559dd849d6b8f8f
NamitsGit <73180333+NamitsGit@users.noreply.github.com>
2022-05-17 06:47:02 +0530

    Update extract_text.py

--- extract_text.py
+++ extract_text.py
@@ -9,22 +9,30 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
 
 output_string = StringIO()
-inp_file = "statement_sample1-Copy1.PDF"
 
-with open(inp_file, 'rb') as in_file:
-    parser = PDFParser(in_file)
-    doc = PDFDocument(parser)
-    rsrcmgr = PDFResourceManager()
-    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    for page in PDFPage.create_pages(doc):
-        interpreter.process_page(page)
+def extract_text_from_pdf(filename):
+    inp_file = filename
 
+    if not os.path.exists(inp_file):
+        raise Exception("The given input path \'"+inp_file+"\' of the pdf file does not exist.\nRe-enter the file path and try again.")
 
-print(output_string.getvalue())
+    if not os.path.isfile(inp_file):
+        raise Exception("The given input path \'"+inp_file+"\' does not correspond to a filename.\nRe-enter the file path to include the proper filename.")
 
-out_file = 'extracted_' + os.path.basename(inp_file)[:-4] +'.txt'
-with open(os.path.basename(out_file), mode='w') as f:
-    print(output_string.getvalue(), file=f)
+    with open(inp_file, 'rb') as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
 
-print("The file written is :   "+out_file)
+    out_file = 'extracted_' + os.path.basename(inp_file)[:-4] +'.txt'
+    with open(os.path.basename(out_file), mode='w') as f:
+        print(output_string.getvalue(), file=f)
+
+    print("The file written is :   "+out_file)
+
+# SAMPLE
+# extract_text_from_pdf('test/statement_sample1-Copy1.pdf')
